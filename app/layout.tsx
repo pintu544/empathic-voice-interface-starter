dbc98345d2959a37fbead44596500191ce5308b8
Pintu Kumar <64580363+pintu544@users.noreply.github.com>
2025-09-07 22:13:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Hume AI - EVI",
+  description: "A  Hume AI's Empathic Voice Interface",
 };
 
 export default function RootLayout({
